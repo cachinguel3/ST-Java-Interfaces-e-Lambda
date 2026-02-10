ed38c428da999a5a50e10419c29c7eebbd8caece
Eduardo_A. <edu.crecenzio.1003@gmail.com>
2026-02-10 15:09:51 -0300

    Primeiro exemplos de interfaces e lambda

---
+++ Interfaces/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Computer musicPlayer = new Computer();
+        runMusic(musicPlayer);
+        runVideo(musicPlayer);
+    }
+
+    public static void runVideo(VideoPlayer videoPlayer) {
+        videoPlayer.playVideo();
+    }
+
+    public static void runMusic(MusicPlayer musicPlayer) {
+        musicPlayer.playMusic();
+    }
+}
